98433ac1492e362a0753ed0d4fb80b44fb1a3b73
Akash M K <104432721+akashmk332@users.noreply.github.com>
2022-08-25 12:01:16 +0530

    Add files via upload
    Second Assignment in React

---
+++ Grid Design/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./footer.module.css";
+
+const footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className="text">
+        <h1>Edyoda Digital University</h1>
+      </div>
+    </footer>
+  );
+};
+
+export default footer;
