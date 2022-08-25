256d5a4d7a93b95c8923f70b6fb4baa1366170fe
Akash M K <104432721+akashmk332@users.noreply.github.com>
2022-08-25 14:38:29 +0530

    Add files via upload
    Third Assignment in React

---
+++ Movie-App/src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import React from "react";
+import axios from "axios";
+import Card from "./Card/Card";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movie: [],
+    };
+  }
+
+  componentDidMount = async () => {
+    try {
+      const response = await axios.get(
+        "https://www.omdbapi.com/?apikey=45f0782a&s=war"
+      );
+      // console.log(response.data.Search);
+      this.setState({
+        movie: response.data.Search,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <Card movieList={this.state.movie} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Movie-App/src/Card/Card.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./Card.css";
+
+class Card extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      filterMovies: [],
+    };
+  }
+
+  filterMovieItems(e) {
+    this.setState(
+      {
+        searchQuery: e.target.value,
+        filterMovies: this.props.movieList.filter((movie, i) =>
+          movie.Title.toLowerCase().includes(e.target.value.toLowerCase())
+        ),
+      },
+      () => {
+        console.log(this.state.filterMovies);
+      }
+    );
+  }
+
+  render() {
+    let { movieList } = this.props;
+    return (
+      <>
+        <div className="container">
+          <h1>React Movie App</h1>
+          <div className="search">
+            <input
+              type="search"
+              value={this.state.searchQuery || ""}
+              onChange={(e) => this.filterMovieItems(e)}
+              placeholder="Search Movies......"
+            ></input>
+          </div>
+        </div>
+        <div className="moviecontainer">
+          {this.state.filterMovies.length <= 0
+            ? movieList.map((movie, i) => (
+                <div className="moviecard" key={i}>
+                  <img src={movie.Poster} alt={i} />
+                  <div className="moviedesc">
+                    <h3>{movie.Title}</h3>
+                  </div>
+                </div>
+              ))
+            : this.state.filterMovies.map((movie, i) => (
+                <div className="moviecard" key={i}>
+                  <img src={movie.Poster} alt={i} />
+                  <div className="moviedesc">
+                    <h3>{movie.Title}</h3>
+                  </div>
+                </div>
+              ))}
+        </div>
+      </>
+    );
+  }
+}
+
+export default Card;
